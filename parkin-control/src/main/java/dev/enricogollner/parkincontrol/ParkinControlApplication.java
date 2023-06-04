8e172b03a57cba3f551533b534c97c7f14a0f7f9
EnricoGollner <enricogollner.tech@gmail.com>
2023-06-04 17:00:11 -0300

    Starting project

---
+++ parkin-control/src/main/java/dev/enricogollner/parkincontrol/ParkinControlApplication.java
@@ -0,0 +1,13 @@
+package dev.enricogollner.parkincontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkinControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkinControlApplication.class, args);
+	}
+
+}
---
+++ parkin-control/src/test/java/dev/enricogollner/parkincontrol/ParkinControlApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.enricogollner.parkincontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkinControlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
